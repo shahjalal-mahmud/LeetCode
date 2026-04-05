fd97b1875fdae310b6297d1dbd252b3335706997
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-04-05 22:37:59 +0600

    Implement sortedArrayToBST function to convert a sorted array into a height-balanced binary search tree

---
+++ Array/108_Convert_Sorted_Array_to_Binary_Search_Tree.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ * int val;
+ * TreeNode *left;
+ * TreeNode *right;
+ * TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ * TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ * TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return buildBST(nums, 0, nums.size() - 1);
+    }
+
+private:
+    TreeNode* buildBST(vector<int>& nums, int left, int right) {
+        // Base case: if the range is invalid, this branch is empty
+        if (left > right) {
+            return nullptr;
+        }
+
+        // Choose the middle element to ensure height balance
+        int mid = left + (right - left) / 2;
+        
+        // Create the root node with the middle value
+        TreeNode* root = new TreeNode(nums[mid]);
+
+        // Recursively build the left and right subtrees
+        root->left = buildBST(nums, left, mid - 1);
+        root->right = buildBST(nums, mid + 1, right);
+
+        return root;
+    }
+};
