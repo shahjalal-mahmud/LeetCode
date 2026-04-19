292edc09580be294ec4ebb074a1b65ea42904b72
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-04-19 22:29:13 +0600

    Implement isPalindrome function to check if a string is a palindrome, ignoring non-alphanumeric characters and case

---
+++ String/125_Valid_Palindrome.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right) {
+            // Skip non-alphanumeric characters from the left
+            while (left < right && !isalnum(s[left])) {
+                left++;
+            }
+            // Skip non-alphanumeric characters from the right
+            while (left < right && !isalnum(s[right])) {
+                right--;
+            }
+            
+            // Compare characters (case-insensitive)
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+            
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+};
